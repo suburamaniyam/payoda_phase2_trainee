dca6467ff760f21faec56ee8408dc1feb20bdab2
suburamaniyam <keerthanasubu73@gmail.com>
2024-09-06 15:22:24 +0530

    Add files via upload

---
+++ phase 2 trainne daily task/JWTauthendicatoin/Model/UserDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JWTauthendicatoin.Model
+{
+    public class UserDbContext:DbContext
+    {
+        public DbSet<Student>? students { get; set; }
+        public DbSet<User>? Users { get; set; }
+
+
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+
+        {
+
+        }
+    }
+}
+
+    
+
+
+    
+
---
+++ phase 2 trainne daily task/JWTauthendicatoin/Program.cs
@@ -0,0 +1,34 @@
+
+using JWTauthendicatoin.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<UserDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("payoda")));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
