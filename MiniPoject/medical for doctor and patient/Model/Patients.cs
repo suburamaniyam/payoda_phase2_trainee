ef2fa76d4897b74a2c7af5cb18bfd1db1cab74aa
suburamaniyam <keerthanasubu73@gmail.com>
2024-09-12 17:19:50 +0530

    Add files via upload

---
+++ MiniPoject/medical for doctor and patient/Model/Patients.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedicineOfDoctorPatients.Model
+{
+    public class Patients
+    {
+        [Key]
+        public int Patid { get; set; }
+        public string? Patname { get; set; }
+        public int Docid { get; set; }
+        //navigator Patients Appointments
+        [ForeignKey("Docid")]
+        public Doctor? Doctors { get; set; }
+    }
+}
